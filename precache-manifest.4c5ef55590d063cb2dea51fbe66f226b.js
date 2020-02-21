2e5984a158f2021f55d090d4a4d8a749d3634ab6
PeterGrillot <petergrillot@gmail.com>
2020-02-20 19:01:32 -0500

    Updates

---
+++ precache-manifest.4c5ef55590d063cb2dea51fbe66f226b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0619e1f0b7a897a6b05af21b860fa0b",
+    "url": "/jumprope/index.html"
+  },
+  {
+    "revision": "a207438900f3bb832fc4",
+    "url": "/jumprope/static/css/main.71ab10c6.chunk.css"
+  },
+  {
+    "revision": "551a81d6eef3f19d2d51",
+    "url": "/jumprope/static/js/2.3c156ca5.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/jumprope/static/js/2.3c156ca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a207438900f3bb832fc4",
+    "url": "/jumprope/static/js/main.1ff4c0e7.chunk.js"
+  },
+  {
+    "revision": "a9c8b5422a96961cbef5",
+    "url": "/jumprope/static/js/runtime-main.7666d85f.js"
+  },
+  {
+    "revision": "540ae400e1c195e274bca8351d506c9e",
+    "url": "/jumprope/static/media/jumprope.540ae400.svg"
+  }
+]);
